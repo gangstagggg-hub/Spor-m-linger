496eeef5b0061b6e34e16bf545927eaa00310041
gangstagggg-hub <gangstagggg@gmail.com>
2026-08-19 14:14:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAVN = "gedo-tralle-v1";
+const CACHE_NAVN = "gedo-tralle-v3";
 const APP_SKALL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
